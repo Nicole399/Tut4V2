fe5ed1d30ced6b7a77f1d409060e0bea0e3dbd36
Nicole Stone <87792051+Nicole399@users.noreply.github.com>
2021-08-14 17:22:25 +1200

    Update App.java

--- src/main/java/Tut4v2/App.java
+++ src/main/java/Tut4v2/App.java
@@ -2,14 +2,14 @@ package Tut4v2;
 
 public class App 
 {
-   public class Calc {
+   public static class Calc {
 	
-	public int add(int x, int y) {
+	public static int add(int x, int y) {
 		return x+y;
 		
 	}
 	
-	public int subtract(int x, int y) {
+	public static int subtract(int x, int y) {
 		return x+y;
 		
 	}
