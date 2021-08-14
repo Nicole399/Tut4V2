8ad52b4418fd9473d54c674a1bb49c24734710dd
Nicole Stone <slimline.nicole@gmail.com>
2021-08-14 16:15:01 +1200

    aldding all filed

---
+++ Tutorial4v2/src/main/java/Tut4v2/App.java
@@ -0,0 +1,23 @@
+package Tut4v2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+   public class Calc {
+	
+	public static int add(int x, int y) {
+		return x+y;
+		
+	}
+	
+	public static int subtract(int x, int y) {
+		return x+y;
+		
+	}
+
+}
+
+
