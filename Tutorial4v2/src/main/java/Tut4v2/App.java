a6deee2da52e7816fae2663619a71f8ce68f339b
Nicole Stone <slimline.nicole@gmail.com>
2021-08-14 16:24:48 +1200

    #Please enter the commit message for your changes. Lines starting  updated pom and app

--- Tutorial4v2/src/main/java/Tut4v2/App.java
+++ Tutorial4v2/src/main/java/Tut4v2/App.java
@@ -1,23 +1,19 @@
 package Tut4v2;
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
    public class Calc {
 	
-	public static int add(int x, int y) {
+	public int add(int x, int y) {
 		return x+y;
 		
 	}
 	
-	public static int subtract(int x, int y) {
+	public int subtract(int x, int y) {
 		return x+y;
 		
 	}
 
 }
-
+}
 
